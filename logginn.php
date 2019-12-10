cf3879e10bbf5cde1693818d833d39952c09f7d7
EliasNesheim <elias.nesheim@gmail.com>
2019-12-10 12:56:40 +0100

    Update logginn.php

--- logginn.php
+++ logginn.php
@@ -7,8 +7,6 @@
 	
 	$USER = stripcslashes($USER);
 	$PASS = stripcslashes($PASS);
-	// $USER = mysql_real_escape_string($USER);
-	// $USER = mysql_real_escape_string($PASS);
 	
 	$PW= sha1($SALT.$PASS);
 	
@@ -17,20 +15,96 @@
 		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 	}
 	echo $mysqli->host_info . "\n";
-	
-	
-	$RESULT = mysqli_query($mysqli, "select * from bruker where epost = '{$USER}' and passord = '{$PW}'");
-	$ROW = mysqli_fetch_array($RESULT);
-	
-	if ($ROW['epost'] == $USER && $ROW['passord'] == $PW) {
-		header('Location: backend.php');
-		$_SESSIONS['bruker'] = $bruker;
-		$_SESSIONS['brukertype'] = $row['brukertype'];
+
+		//Sjekk sisteinnlogging
+		if (!($stmt = $mysqli->prepare('SELECT feillogginnsiste FROM bruker WHERE brukernavn = ? ;'))) {
+			echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+		}
 		
-	}
-	
-	else {
-		echo $ROW,' ', $USER,' ', $PW;
-         //header("Location: uvelkommen.html");
-	}
+		if (!$stmt->bind_param('s',$epost)) {
+			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+
+		if (!$stmt->execute()) {
+			echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+		//echo $stmt;
+		//while (stmt->fetch)
+		$Resultat = mysqli_stmt_fetch($stmt);
+		mysqli_stmt_close($stmt);
+		echo ' ',$Resultat;
+		
+
+		$timeNow = date('Y-m-d H:i:s', strtotime('-5 minutes'));
+		if ($Resultat < $timeNow) {
+			
+
+			// reset counter
+			if (!($stmt = $mysqli->prepare('UPDATE bruker SET feillogginnteller = 0 WHERE brukernavn = ? ;'))) {
+				echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+			}
+			
+			if (!$stmt->bind_param('s',$epost)) {
+				echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+			}
+
+			if (!$stmt->execute()) {
+				echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+			}
+				// innloggingsforsøk +1
+
+				if (!($stmt = $mysqli->prepare('UPDATE bruker SET feillogginnteller = feillogginnteller+1 WHERE brukernavn = ? ;'))) {
+					echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+				}
+				
+				if (!$stmt->bind_param('s',$epost)) {
+					echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+				}
+
+				if (!$stmt->execute()) {
+					echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+				}
+			
+					// sjekk antall innloggingsforsøk
+
+					if (!($stmt = $mysqli->prepare('SELECT * FROM bruker WHERE brukernavn =  ? ;'))) {
+						echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+					}
+					if (!$stmt->bind_param('s',$epost)) {
+						echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+					}
+				
+					if (!$stmt->execute()) {
+						echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+					}
+					$Resultat = mysqli_stmt_get_result($stmt);
+
+					$ROW = mysqli_fetch_assoc($Resultat);
+			
+					echo $ROW;
+
+						if ($ROW['feillogginnteller'] < 5) {
+
+							// $RESULT = mysqli_query($mysqli, "select * from bruker where epost = '{$USER}' and passord = '{$PW}'");
+
+							if ($ROW['epost'] == $USER && $ROW['passord'] == $PW) {
+								header('Location: backend.php');
+								$_SESSIONS['bruker'] = $bruker;
+								$_SESSIONS['brukertype'] = $row['brukertype'];
+								
+							}
+							
+							else {
+								echo $ROW,' ', $USER,' ', $PW;
+								//header("Location: uvelkommen.html");
+							}
+						}
+						else {
+							echo 'Feillogginnteller er ikke < 5';
+						}
+
+		}
+		else {
+			echo 'Du har forsøk å logg inn for mange ganger';
+		}
 ?>
