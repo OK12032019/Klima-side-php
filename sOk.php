977939e268f246679ddad62745e31c6f7274e502
OK12032019 <48478599+OK12032019@users.noreply.github.com>
2020-02-07 17:33:33 +0100

    Update sOk.php

--- sOk.php
+++ sOk.php
@@ -27,7 +27,47 @@ if(isset($_POST['btn-logout']))
 
 ?>
 <html>
+<head>
+    <link rel="stylesheet" href="FellesCSS.css">
+    <title>Søk på Bruker</title>
+</head>
 <body>
+
+    <header class="hovedheader">
+        <a href="default.php" class="logoen"><img src="n.png"style="width:80px;"></a>
+        <input class="menu-btn" type="checkbox" id="menu-btn" />
+        <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+		
+		            <a href="#" class="w3-bar-item" title="Konto">
+                <a href="Brukerside.php"<img src="Bruker.png" class="w3-circle" style="height:28px;width:38px" alt="Avatar"> </a>
+            </a>
+			
+        <ul class="menu">
+		
+            <a href="#" class="logoen1">Artikler</a>
+			
+            <a href="Brukerside.php" class="logoen2">Profil</a>
+			
+			
+            <a href="#" class="logoen3">Arrangementer</a>
+			
+			
+
+			
+              <a href="Passord.php" class="nullpass">Nullstill Passord</a>
+			
+			
+        <div class="a123">
+        <form method="post">
+            <button type="submit" name="btn-logout" class="btn btn-block btn-primary">
+                <i class="glyphicon glyphicon-log-in"></i>&nbsp;Logg ut
+            </button>
+            </form>
+        </div>
+        </ul>   
+
+    </header>
+
 <main>
 
 <br>
@@ -61,18 +101,19 @@ if(isset($_POST['registrer']))
 <div class="container">
 <div class="form-container">
         <form method="post">
-        <h2>Søk.</h2><hr />
+        <h2 id="Mellomrom4">Søk.</h2><hr />
         <div class="form-group">
-            <input type="text" class="form-control" name="brukernavn" placeholder="Enter Username" />
+            <input type="text" class="form-control" name="brukernavn" placeholder="Enter Username" id="Mellomrom5"/>
             </div>
             <div class="clearfix"></div><hr />
             <div class="form-group">
              <button type="submit" class="btn btn-block btn-primary" name="registrer">
-                 <i class="glyphicon glyphicon-open-file"></i>&nbsp;SIGN UP
+                 <i class="glyphicon glyphicon-open-file"></i>&nbsp;Søk
                 </button>
             </div>
             </form>
     
 </main>
 </body>
+
 </html>
