aafafe448f6a7d7d5a8d1b07f90e2fed05ddfc08
OK12032019 <48478599+OK12032019@users.noreply.github.com>
2020-03-06 19:32:26 +0100

    Update minmeny.php

--- minmeny.php
+++ minmeny.php
@@ -17,7 +17,7 @@ if($btype == 1){
             <li><a href="Backend.php" class="mellomrom1">Artikler</a></li>
 			 <li><a href="Brukerside.php" class="mellomrom1">Profil</a></li>
 			 <li><a href="Sok.php" class="mellomrom2">Administrere brukere</a></li>
-			 
+			 <li><a href="Meldinger.php" class="mellomrom1">Meldinger</a></li>
 			 <div class="e123">
             <form method="post">
         <button type="submit" name="btn-logout" class="btn1 btn-block btn-primary">
@@ -42,6 +42,7 @@ else if($btype == 2){
         <ul class="menu">
             <li><a href="Brukerside.php" class="mellomrom1">Skrive artikler</a></li>
 			 <li><a href="Backend.php" class="mellomrom1">Meny</a></li>
+			 <li><a href="Meldinger.php" class="mellomrom1">Meldinger</a></li>
 			 <div class="e123">
             <form method="post">
         <button type="submit" name="btn-logout" class="btn1 btn-block btn-primary">
@@ -69,6 +70,7 @@ else if($btype == 2){
             <a href="Backend.php" class="logoen1">Artikler</a>
             <a href="Brukerside.php" class="logoen2">Profil</a>
             <a href="Backend.php" class="logoen3">Arrangementer</a>
+		<a href="Meldinger.php" class="mellomrom1">Meldinger</a>
               <a href="Passord.php" class="nullpass">Nullstill Passord</a>
         <div class="a123">
         <form method="post">
